48c8a3fa8c316de600714453f57215c0f20178a5
kimkundad <kim.kundad@gmail.com>
2021-01-06 02:11:10 +0700

    no message

--- app/Http/Controllers/API/CommonController.php
+++ app/Http/Controllers/API/CommonController.php
@@ -1049,12 +1049,13 @@ class CommonController extends Controller
                                     if ($topHead == '1X2') {
                                         $teamDrawText = 'Draw';
                                         $teamDrawScore = null;
-                                      //  dd($row);
+                                        dd($row);
                                       //  if (array_key_exists('left', $row)) {
 
-                                            $teamLeftName = $row['left'][0];
+                                            $teamLeftName = $row['match_datas']['left'][0];
+                                            dd($teamLeftName);
                                             $teamLeftRight = $row['left'][1];
-                                            dd($teamLeftRight);
+
                                       //  }
                                         if (array_key_exists('mid', $row)) {
                                             $teamDrawText = $row['mid'][0];
