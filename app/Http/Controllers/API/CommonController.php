c139bbb14be4a433cd320c841f8aa33d2a2f4a41
kimkundad <kim.kundad@gmail.com>
2021-01-06 02:33:58 +0700

    no message

--- app/Http/Controllers/API/CommonController.php
+++ app/Http/Controllers/API/CommonController.php
@@ -1051,7 +1051,7 @@ class CommonController extends Controller
                                         $teamDrawScore = null;
                                       //  dd($row);
                                       //  if (array_key_exists('left', $row)) {
-                                            dd($row[0]->match_datas);
+                                            dd($row->match_datas[0]);
                                             $teamLeftName = $row['left'][0];
                                             $teamLeftRight = $row['left'][1];
 
